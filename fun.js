60bc2b19051cb9ba898419221b67302729a815ce
yoboi1 <56106042+yoboi1@users.noreply.github.com>
2026-02-24 17:45:47 -0500

    Create fun.js

---
+++ fun.js
@@ -0,0 +1,20 @@
+const h1 = document.querySelector("h1");
+
+let y = 0;
+const speed = 2;
+
+h1.style.position = "fixed";
+h1.style.left = "50%";
+h1.style.transform = "translateX(-50%)";
+
+function move() {
+  y += speed;
+
+  if (y > window.innerHeight) y = -h1.offsetHeight;
+
+  h1.style.top = y + "px";
+
+  requestAnimationFrame(move);
+}
+
+move();
